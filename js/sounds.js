d9a939687b62f8b17f1807d7604ae88a09e5219d
Being-RAYtastic <103839857+Being-RAYtastic@users.noreply.github.com>
2022-07-08 17:42:20 +0530

    Increased Volume of Sounds

--- js/sounds.js
+++ js/sounds.js
@@ -1,16 +1,16 @@
 const background_music = document.querySelector('#background-music');
-background_music.volume = 0.2;
+background_music.volume = 0.5;
 background_music.autoplay = true;
 
 const attack_sound = document.querySelector('#attack-sound');
 attack_sound.volume = 0.4;
 
 const tie_sound = document.querySelector('#tie-sound');
-tie_sound.volume = 0.1;
+tie_sound.volume = 0.2;
 
 const game_over = document.querySelector('#game-over');
-game_over.volume = 0.1;
+game_over.volume = 0.2;
 
 
 const emotional_damage_meme = document.querySelector('#emotional-damage-sound')
-emotional_damage_meme.volume = 0.3;
+emotional_damage_meme.volume = 0.4;
