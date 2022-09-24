8667e89be0961c9ad26f7ade0df1fcab0f6bca88
Risheet Ray <103839857+Being-RAYtastic@users.noreply.github.com>
2022-09-24 22:38:02 +0530

    Delete oobe.js

--- js/oobe.js
+++
@@ -1,9 +0,0 @@
-const play_btn = document.getElementById('play');
-play_btn.addEventListener('click', ()=>{
-    window.location = 'theLastOne.html';
-})
-
-const help_btn = document.getElementById('help');
-help_btn.addEventListener('click', ()=>{
-    window.location = 'help.html';
-})
