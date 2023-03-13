f5c4f41f1e0c7ab0f1e2a76fb7f532415e92feb2
Being-RAYtastic <103839857+Being-RAYtastic@users.noreply.github.com>
2023-03-13 13:21:01 +0530

    edited sound volumes

--- js/sounds.js
+++ js/sounds.js
@@ -8,11 +8,11 @@ const attack_sound = document.querySelector('#attack-sound');
 attack_sound.volume = 0.4;
 
 const tie_sound = document.querySelector('#tie-sound');
-tie_sound.volume = 0.2;
+tie_sound.volume = 0.1;
 
 const game_over = document.querySelector('#game-over');
-game_over.volume = 0.2;
+game_over.volume = 0.1;
 
 
 const emotional_damage_meme = document.querySelector('#emotional-damage-sound')
-emotional_damage_meme.volume = 0.4;
+emotional_damage_meme.volume = 0.2;
